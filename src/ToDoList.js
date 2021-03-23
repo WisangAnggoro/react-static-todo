78b5bd0c180ba1c6feda0a6d11c22c93ee1ddb20
Wisang Jati Anggoro <wisang1999@gmail.com>
2021-03-23 11:21:08 +0700

    add hover css to edit and delete button

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -6,17 +6,17 @@ export function ToDoList(props) {
       {
         props.toDoList.map((e,index) => 
           <li className="grid grid-cols-12 gap-1 shadow appearance-none border rounded w-full py-2 px-6 text-gray-700 leading-tight focus:outline-white focus:shadow-outline bg-white mt-1">
-            <div>
+            <div className="text-center">
               <b>{index+1}</b> 
             </div>
-            <div className="col-span-9">
+            <div className="col-span-9 cursor-auto">
               {e}
             </div>
-            <div>
+            <div className="hover:text-yellow-600 cursor-pointer text-center">
               <FaEdit />
             </div>
-            <div>
-              <FaCross 
+            <div className="text-center hover:text-red-600 cursor-pointer">
+              <FaCross
                 onClick={(f) => {
                   f.preventDefault()
                   props.removeToDo(index)
